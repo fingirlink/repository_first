9d8760cf33dd7f566ed3804504bcc0d12189892c
fingirlink <fingirlink@fingirlink.localdomain>
2025-07-09 18:14:20 +0300

    all

--- folder1/file1.go
+++ folder1/file1.go
@@ -1,6 +1,12 @@
 
+package bit
+
 import "fmt"
 
 func Black() {
 	fmt.Println("Прив из first12")
+}
+
+func Second() {
+	fmt.Println("Ещё одна функция")
 }
--- folder1/first.go
+++
@@ -1,6 +0,0 @@
-package bit
-import "fmt"
-
-func Black() {
-	fmt.Println("Привет из first1")
-}
