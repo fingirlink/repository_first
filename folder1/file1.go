7ec31371007a96d9800d3926207b05daa743e5fe
fingirlink <fingirlink@fingirlink.localdomain>
2025-07-09 18:50:03 +0300

    lorem3

--- folder1/file1.go
+++ folder1/file1.go
@@ -7,8 +7,4 @@ func Black() {
 	fmt.Println("Прив из first12")
 }
 
-
-func Second() {
-	fmt.Println("Ещё одна функция")
-}
 //LOREM
--- folder1/first2.go
+++
@@ -1,7 +0,0 @@
-package bit
-import "fmt"
-
-func Second(){
-	fmt.Println("Привет из first2")
-}
-
